93743619431233f7fc4a189b211b218d0bf4800f
Jerrif <6222577+Jerrif@users.noreply.github.com>
2023-12-30 23:39:54 +0800

    fix fish snapping up/down slightly on the frame they spawn

--- Assets/_Game/Scripts/SineMovement.cs
+++ Assets/_Game/Scripts/SineMovement.cs
@@ -7,9 +7,12 @@ public class SineMovement : MonoBehaviour {
     private Vector2 startPos;
     private float randOffset;
 
-    void Start() {
+    void OnEnable() {
         startPos = transform.position;
         randOffset = Random.Range(-Mathf.PI/2, Mathf.PI/2);
+        Vector2 nextPos = transform.position;
+        nextPos.y = Mathf.Sin(Time.time * frequency + randOffset) * amplitude + startPos.y;
+        transform.position = nextPos;
     }
 
     void Update() {
