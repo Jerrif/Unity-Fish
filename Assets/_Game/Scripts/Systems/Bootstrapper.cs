b4b7e9aaa96df61177355dc7184ef31042dbc5b7
Jerrif <6222577+Jerrif@users.noreply.github.com>
2024-02-11 06:42:14 +0800

    add bootstrapper system to preload persistent systems

---
+++ Assets/_Game/Scripts/Systems/Bootstrapper.cs
@@ -0,0 +1,11 @@
+// From this video (Project Initialization - Unity Architecture - Tarodev): https://www.youtube.com/watch?v=zJOxWmVveXU
+// This script automatically preloads `Systems` from the `Resources` folder (which is a `don't destroy on load` singleton).
+// I don't love doing it this way (seems a little over complex?), but I think Unity is just shit in this regard,
+// and maybe this is just the least annoying way to do it.
+
+using UnityEngine;
+
+public static class Bootstrapper {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("Systems")));
+}
