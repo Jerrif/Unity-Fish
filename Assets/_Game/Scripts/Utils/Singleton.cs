cc839c393000a6bf2868a9fd3a752fef60260633
Jerrif <6222577+Jerrif@users.noreply.github.com>
2024-02-11 06:53:53 +0800

    {

--- Assets/_Game/Scripts/Utils/Singleton.cs
+++ Assets/_Game/Scripts/Utils/Singleton.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 /// </summary>
 public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour {
     public static T Instance { get; private set; }
-    protected virtual void Awake(){
+    protected virtual void Awake() {
         Instance = this as T;
     }
 
