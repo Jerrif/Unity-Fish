36ac28859a42429eebd6308458c18647babfe0fe
Jerrif <6222577+Jerrif@users.noreply.github.com>
2024-02-10 10:36:35 +0800

    began work on game state with `GameManager`

---
+++ Assets/_Game/Scripts/Systems/GameManager.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Instance;
+
+    [SerializeField] float gameLength = 60f;
+    // [SerializeField] Timer timer;
+
+    public GameState state;
+    public static event Action<GameState> gameStateChanged;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+        // timer.timerExpired += TimeIsUp;
+    }
+
+    private void Start() {
+        UpdateGameState(GameState.MAIN_MENU);
+        // timer.StartTimer(gameLength);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+    private void TimeIsUp() {
+        print("waow time is up");
+        // SceneSystem.Instance.LoadScene(0);
+    }
+
+    public void UpdateGameState(GameState newState) {
+        if (newState == state) return;
+        state = newState;
+
+        switch (newState) {
+            case GameState.MAIN_MENU:
+            break;
+            case GameState.GAME_START:
+            break;
+            case GameState.PAUSED:
+            break;
+            case GameState.SETTINGS_MENU:
+            break;
+            case GameState.GAME_OVER:
+            break;
+        }
+
+        gameStateChanged?.Invoke(newState);
+    }
+}
+
+public enum GameState {
+    MAIN_MENU,
+    GAME_START,
+    PAUSED,
+    SETTINGS_MENU,
+    GAME_OVER
+}
---
+++ Assets/_Game/Scripts/Systems/UISystem.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class UISystem : MonoBehaviour {
+
+    private void Awake() {
+        GameManager.gameStateChanged += OnGameStateChanged;
+    }
+
+    private void OnDestroy() {
+        GameManager.gameStateChanged -= OnGameStateChanged;
+    }
+
+    private void OnGameStateChanged(GameState newState) {
+        print("waow new state dood! " + newState.ToString());
+    }
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+
+}
+
+        
+// Game Manager - Controlling the flow of your game [Unity Tutorial]
+// https://www.youtube.com/watch?v=4I0vonyqMi8&t=79s
+// https://www.youtube.com/watch?v=zJOxWmVveXU
+// https://www.youtube.com/watch?v=tE1qH8OxO2Y&t=356s
