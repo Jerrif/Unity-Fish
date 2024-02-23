214a03ac3033c5ec6dc8050c5cd2bfb020c11dd6
Jerrif <6222577+Jerrif@users.noreply.github.com>
2024-02-23 09:16:25 +0800

    solved the auto screen transition problem super easily using coroutines

--- Assets/_Game/Scripts/ScreenTransition.cs
+++ Assets/_Game/Scripts/ScreenTransition.cs
@@ -1,45 +1,36 @@
-using System;
 using UnityEngine;
 
-// TODO: Maybe I can simplify this and combine `SimpleBlit.cs` into here?
-// Or at least set the material in `SimpleBlit` from here. Or something.
 public class ScreenTransition : MonoBehaviour {
     [SerializeField] private Material transitionMaterial;
     [SerializeField] private float transitionSpeed = 1f;
-    private float fadeTarget = 0f;
-    private float fadeCurrent = 0f;
-    // private bool fading = false;
+    public float fadeTarget { get; private set; } = 0f;
+    public float fadeCurrent { get; private set; } = 0f;
 
-    public event Action fadeComplete;
+    // public event Action fadeOutComplete;
+    // public event Action fadeInComplete;
 
     private void OnEnable() {
         fadeTarget = 0f;
         fadeCurrent = 0f;
-        // fading = false;
     }
 
     private void Update() {
+        // TODO: delete these, obviously
         if (Input.GetKeyDown(KeyCode.Z)) FadeOut();
         if (Input.GetKeyDown(KeyCode.X)) FadeIn();
-
-        // if (!fading) return;
-        // if (fadeCurrent < fadeTarget) {
-        //     // currentFade_t += Time.deltaTime;
-        //     fadeCurrent = Mathf.MoveTowards(fadeCurrent, fadeTarget, transitionSpeed * Time.deltaTime);
-        // }
 
         if (fadeCurrent < fadeTarget) {
             fadeCurrent += transitionSpeed * Time.deltaTime;
             if (fadeCurrent > fadeTarget) {
                 fadeCurrent = fadeTarget;
-                fadeComplete?.Invoke();
+                // fadeOutComplete?.Invoke();
             }
             transitionMaterial.SetFloat("_Cutoff", fadeCurrent);
         } else if (fadeCurrent > fadeTarget) {
             fadeCurrent -= transitionSpeed * Time.deltaTime;
             if (fadeCurrent < 0f) {
                 fadeCurrent = 0f;
-                fadeComplete?.Invoke();
+                // fadeInComplete?.Invoke();
             }
             transitionMaterial.SetFloat("_Cutoff", fadeCurrent);
         }
--- Assets/_Game/Scripts/Systems/UISystem.cs
+++ Assets/_Game/Scripts/Systems/UISystem.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +8,6 @@ public class UISystem : Singleton<UISystem> {
     [SerializeField] private GameObject gameUI;
     private ScreenTransition transitionController;
 
-    private event Action afterFade;
-
     protected override void Awake() {
         base.Awake();
         transitionController = GetComponent<ScreenTransition>();
@@ -17,12 +15,14 @@ public class UISystem : Singleton<UISystem> {
 
     private void OnEnable() {
         GameManager.gameStateChanged += OnGameStateChanged;
-        transitionController.fadeComplete += OnFadeComplete;
+        // transitionController.fadeOutComplete += OnFadeOutComplete;
+        // transitionController.fadeInComplete += OnFadeInComplete;
     }
 
     private void OnDestroy() {
         GameManager.gameStateChanged -= OnGameStateChanged;
-        transitionController.fadeComplete -= OnFadeComplete;
+        // transitionController.fadeOutComplete -= OnFadeOutComplete;
+        // transitionController.fadeInComplete -= OnFadeInComplete;
     }
 
     private void OnGameStateChanged(GameState newState) {
@@ -32,40 +32,32 @@ public class UISystem : Singleton<UISystem> {
     //     mainMenuPanel.SetActive(newState == GameState.MAIN_MENU);
     }
 
-    /* 
-        play button pressed
-        disable play button
-        start fading out
-        on fade out complete:
-            unload old ui
-            load new ui
-        start fading in
-        on fade in complete:
-            update game state
-        ??? profit
+    // TODO: Use coroutines?
+    // https://gamedevbeginner.com/coroutines-in-unity-when-and-how-to-use-them/
+    // https://www.youtube.com/watch?v=t4m8pmahbzY
+    // https://www.youtube.com/watch?v=7RBI9mb8s3E
+    // maybe combine it with Actions like in the last video
 
-        I thought it'd be easy with the use of `Events`, and I kind of do have it working, but IMO it's pretty awful right now.
-        I might try to refactor the `UIManager` to just use an update loop? Need to think about it a little more
 
-    */
-
-    public void PlayButtonPressed() {
-        // GameManager.Instance.UpdateGameState(GameState.GAME_START);
-        playButton.enabled = false; // IMPORTANT: remember to re-activate this after
-        afterFade = FadeToGame;
+    private IEnumerator LoadGameUI() {
         transitionController.FadeOut();
-        // afterFade = GameManager.Instance.UpdateGameState(GameState.GAME_START);
-    }
-
-    private void FadeToGame() {
+        while (transitionController.fadeCurrent != transitionController.fadeTarget) {
+            yield return null;
+        }
+        playButton.enabled = true;
         ShowMainMenu(false);
         ShowGameUI(true);
-        // afterFade = null;
+        yield return new WaitForSeconds(0.5f);
         transitionController.FadeIn();
+        while (transitionController.fadeCurrent != transitionController.fadeTarget) {
+            yield return null;
+        }
+        // TODO: GameMangager.updatestate to new state
     }
 
-    private void OnFadeComplete() {
-        afterFade.Invoke();
+    public void PlayButtonPressed() {
+        playButton.enabled = false;
+        StartCoroutine(LoadGameUI());
     }
 
     public void ShowGameUI(bool active) {
@@ -76,8 +68,3 @@ public class UISystem : Singleton<UISystem> {
         mainMenuUI.SetActive(active);
     }
 }
-
-// Game Manager - Controlling the flow of your game [Unity Tutorial]
-// https://www.youtube.com/watch?v=4I0vonyqMi8
-// https://www.youtube.com/watch?v=zJOxWmVveXU
-// https://www.youtube.com/watch?v=tE1qH8OxO2Y
