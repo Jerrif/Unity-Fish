f479a2b06b6ffdec8e55dbb625d9352fd2d0f81c
Jerrif <6222577+Jerrif@users.noreply.github.com>
2024-03-24 21:06:59 +0800

    comments/notes etc

--- Assets/_Game/Scripts/SimpleBlit.cs
+++ Assets/_Game/Scripts/SimpleBlit.cs
@@ -26,17 +26,22 @@ EDIT; Note that these step DO NOT apply to a Project with URP; sorry, don't know
 2. Drag the "BattleTransitions" Material onto the "TransitionMaterial" slot on "SimpleBlit"
 3. In the "BattleTransitions" Material itself, select which Texture you wish to use and drop it onto the "Transition Texture" slot. Leave the "Texture" slot alone.
 4. If you're using one of the screen-sheering effects, tick the box which says "Distort" otherwise, leave it disabled
-At this point, you should see the effect working in the Game-View Tab by dragging the "Cutoff" slider from 0 - 1 (I went nuts because I was expecting it to preview in the Scene view tab; NOT THE CASE!)
+At this point, you should see the effect working in the Game-View Tab by dragging the "Cutoff" slider from 0 - 1
+(I went nuts because I was expecting it to preview in the Scene view tab; NOT THE CASE!)
 5. To manipulate the value of the "Cutoff" at runtime, you can use the following line of code;
 TransitionMaterial.SetFloat("_Cutoff", cutoffVal);
 - "TransitionMaterial" is the name of the material in the SimpleBlit script
 - SetFloat("_Cutoff" is how you modify properties on a material; in particular, a float
-- "cutoffVal" is a float I defined which I gradually ramp up to 1f in Update or FixedUpdate. This can obviously be changed to anything you want. For those who are curious, I will include the full modification of the script at the bottom of this comment, but first;
+- "cutoffVal" is a float I defined which I gradually ramp up to 1f in Update or FixedUpdate. This can obviously be changed to anything you want.
+For those who are curious, I will include the full modification of the script at the bottom of this comment, but first;
 
 TEXTURE CREATION TIPS =============================
-- Create Gradients which go between pure black and Hex-Code "efefef"; I tried making images that went to pure white, and they didn't "complete" (a part of the screen was left uncovered; feel free to try it with a throw away test to see what I mean)
-- You can achieve a LOT of cool effects in Photoshop by using the effects under Filter > Distort on top of simple gradients. In particular, I recommend messing around with "Ripple", "Twirl", "Wave", and "Zig-Zag"
-- One thing which I did frequently was create an image effect which took up half the screen, then copied it to a new layer, and flipped it to fill the other half of the screen symmetrically
+- Create Gradients which go between pure black and Hex-Code "efefef"; I tried making images that went to pure white, and they didn't "complete"
+(a part of the screen was left uncovered; feel free to try it with a throw away test to see what I mean)
+- You can achieve a LOT of cool effects in Photoshop by using the effects under Filter > Distort on top of simple gradients.
+In particular, I recommend messing around with "Ripple", "Twirl", "Wave", and "Zig-Zag"
+- One thing which I did frequently was create an image effect which took up half the screen, then copied it to a new layer,
+and flipped it to fill the other half of the screen symmetrically
 
 SimpleBlit (Modified; Full script) =============================
 using UnityEngine;
