3a7842176120ba40650ddb9df8f165889369e87a
Jerrif <6222577+Jerrif@users.noreply.github.com>
2024-06-14 18:05:01 +0800

    fix being able to spam click 'play again' while the 'main menu' fade is happening

--- Assets/_Game/Scripts/GameOverUI.cs
+++ Assets/_Game/Scripts/GameOverUI.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 // Trying something a bit different for this UI - explanation in `UISystem.cs -> ShowGameOverUI()`
 public class GameOverUI : MonoBehaviour {
 
     [SerializeField] private TextMeshProUGUI catchesIntText;
     [SerializeField] private TextMeshProUGUI missesIntText;
+    [SerializeField] private Button playAgainButton;
+    [SerializeField] private Button mainMenuButton;
 
     private void Awake() {
         catchesIntText.SetText("999");
@@ -22,5 +25,16 @@ public class GameOverUI : MonoBehaviour {
 
     public void SetMisses(int misses) {
         missesIntText.SetText(misses.ToString());
+    }
+
+    private void OnEnable() {
+        playAgainButton.interactable = true;
+        mainMenuButton.interactable = true;
+    }
+
+    public void DisableButtons() {
+        print("wow playing again");
+        playAgainButton.interactable = false;
+        mainMenuButton.interactable = false;
     }
 }
