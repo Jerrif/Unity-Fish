223b163c9041c43ba2b18d2b8d7a8791df68032b
Jerrif <6222577+Jerrif@users.noreply.github.com>
2024-06-16 21:25:45 +0800

    add BGM controller & fade in/out

---
+++ Assets/_Game/Scripts/BGMController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BGMController : MonoBehaviour {
+    private AudioSource audioSource;
+    [SerializeField] private float volume;
+    [SerializeField] private float fadeDuration;
+    [SerializeField] private AudioClip mainMenuBGM;
+    [SerializeField] private AudioClip gameRunningBGM;
+    [SerializeField] private AudioClip gameOverBGM;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = volume;
+    }
+
+    private void OnEnable() {
+        GameManager.gameStateChanged += OnGameStateChanged;
+    }
+
+    private void OnDisable() {
+        GameManager.gameStateChanged -= OnGameStateChanged;
+    }
+
+    private void OnGameStateChanged(GameState newState) {
+        AudioClip nextBGM;
+        if (newState == GameState.MAIN_MENU) {
+            nextBGM = mainMenuBGM;
+        } else if (newState == GameState.GAME_START) {
+            nextBGM = gameRunningBGM;
+        } else if (newState == GameState.GAME_OVER) {
+            nextBGM = gameOverBGM;
+        } else {
+            return;
+        }
+
+        StartCoroutine(FadeTo(nextBGM));
+    }
+
+    private IEnumerator FadeTo(AudioClip nextBGM) {
+        yield return StartCoroutine(FadeOut());
+        StartCoroutine(FadeIn(nextBGM));
+    }
+
+    private IEnumerator FadeIn(AudioClip nextBGM) {
+        audioSource.volume = 0f;
+        float timeElapsed = 0f;
+        audioSource.resource = nextBGM;
+        audioSource.Play();
+
+        while (audioSource.volume < volume) {
+            audioSource.volume = Mathf.Lerp(0f, volume, timeElapsed/fadeDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOut() {
+        // audioSource.volume = volume;
+        float timeElapsed = 0f;
+
+        while (audioSource.volume > 0f && audioSource.isPlaying) {
+            audioSource.volume = Mathf.Lerp(volume, 0f, timeElapsed/fadeDuration);
+            timeElapsed += Time.deltaTime;
+            yield return false;
+        }
+        audioSource.Stop();
+    }
+}
